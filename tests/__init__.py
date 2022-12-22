262d9069f168f39f016613818ea480cf8b0519e4
Fabio Florey <93403866+FabioFlorey@users.noreply.github.com>
2022-12-22 14:55:53 +0100

    general refactoring

---
+++ tests/__init__.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# *-* mode: python; coding: utf-8 *-*
+'''
+=============================================================================
+                       ___  ____ ____  _ ____ ____ ___
+                       |__] |__/ |  |  | |___ |     |
+                       |    |  \ |__| _| |___ |___  |
+   <one line to give the program's name and a brief idea of what it does.> 
+=============================================================================
+Author: Fabio Craig Wimmer Florey                             Version: 0.0.1
+=============================================================================
+                                  ~ NOTICE ~
+ 
+ Project, Copyright © 2022, Fabio Craig Wimmer Florey - All Rights Reserved.
+ 
+     Unless required by applicable law or agreed to in writing, software      
+      distributed under the License is distributed on an "AS IS" BASIS,       
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+
+       See the License for the specific language governing permissions        
+                      and limitations under the License.
+ 
+ License: MIT-0                          Terms and Conditions: ./LICENSE.md
+=============================================================================
+'''
