267535aef46f438ad742bb4e33d55574bebcd9db
Fabio Florey <93403866+FabioFlorey@users.noreply.github.com>
2022-12-25 19:46:19 +0100

    updating codecoverage

---
+++ tests/test_main.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# *-* mode: python; coding: utf-8 *-*
+'''
+=============================================================================
+                           ___ ____ ____ ___ ____ 
+                            |  |___ [__   |  [__  
+                            |  |___ ___]  |  ___]                       
+                   Automated tests for the project's build. 
+=============================================================================
+Author: Fabio Craig Wimmer Florey                             Version: 0.0.1
+=============================================================================
+                                  ~ NOTICE ~
+ 
+ Project, Copyright © 2022, Fabio Craig Wimmer Florey - All Rights Reserved.
+
+     Unless required by applicable law or agreed to in writing, software      
+      distributed under the License is distributed on an "AS IS" BASIS,       
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+
+       See the License for the specific language governing permissions        
+                      and limitations under the License.
+ 
+ License: MIT-0                          Terms and Conditions: ./LICENSE.md
+=============================================================================
+'''
+from __future__ import annotations
