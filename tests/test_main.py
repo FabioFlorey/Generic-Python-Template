e34aa6d959e002337981aa529fc5e563a911359c
Fabio Florey <93403866+FabioFlorey@users.noreply.github.com>
2022-12-27 16:00:10 +0100

    testing tox and black

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # *-* mode: python; coding: utf-8 *-*
-"""
+'''
 =============================================================================
 Project
 =============================================================================
@@ -9,7 +9,7 @@ Automated tests for the project's build.
 :Authors: Fabio Craig Wimmer Florey <fabioflorey@icloud.com>
 :Version: 0.0.1
 :License: MIT-0
-"""
+'''
 
 
 def test_function():
